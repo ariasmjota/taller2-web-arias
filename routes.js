95d9cdf383441113f8a70b2a64b13bc8bdfe041c
unknown <majobel10@hotmail.com>
2019-10-28 07:09:47 -0500

    prueba

--- routes.js
+++ routes.js
@@ -1,35 +1,23 @@
 const assert = require('assert');
 
 function createRoutes (app, db) {
-//Ruta inicial
-app.get('/',function(request, response){
-    response.sendFile(__dirname+'/public/home.html');
-    // seleccionamos la colección que necesitamos
-    const products = clientdb.collection('products');
-
-    // buscamos todos los productos
-    products.find({})
-        // transformamos el cursor a un arreglo
-        .toArray((err, result) => {
-            // asegurarnos de que no hay error
-            assert.equal(null, err);
-
-            //
-            console.log(result);
-        });
-
-});
+    
+    // todas las funciones que interactuen con la base de datos van aquí
+    app.get('/', (request, response) => {
+        response.sendFile(__dirname + '/public/home.html');
 
 
-//Ruta a la tienda
-app.get('/store/', function(request, response) {
+    });
+
+    //Ruta a la tienda
+    app.get('/store', function(request, response) {
 
     //Mongo: buscar documentos (Paso 3)
-    var productos = clientdb.collection('productos');
-    productos.find()
+    var products = db.collection('products');
+    products.find()
 	        .toArray(function(err, docs) {
         var contexto = {
-            listaProductos: docs,
+            productsList: docs,
            
         };
         response.render('store',contexto);
@@ -37,5 +25,7 @@ app.get('/store/', function(request, response) {
 
 });
 
-//Ruta de los filtros
-} module.exports=createRoutes;
+}
+
+module.exports = createRoutes;
+
